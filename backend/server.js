903744bac03eb14bea1cdfdbe17aa3d6b54e11cd
szbytniewski <s.zbytniewski.624@studms.ug.edu.pl>
2024-01-02 20:20:44 +0100

    Dodanie struktury plikow w backendzie

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// app.use(require("./routes/"));
+// Tutaj podajam wszystkie routey
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
